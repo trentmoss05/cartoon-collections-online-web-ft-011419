7206e8a3bbaf3b5014fc4701102af30a4029ecb9
Trent Moss <trentmoss05@gmail.com>
2019-01-15 18:39:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet(calls)# code an argument here
+def summon_captain_planet(planateer_calls)# code an argument here
 
 end
 
